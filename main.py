12a8e270b998130ffaf48bf96c83c4c76da92738
BharatArora2006 <103306646+BharatArora2006@users.noreply.github.com>
2025-01-26 20:35:31 +0530

    main.py

--- main.py
+++ main.py
@@ -61,7 +61,9 @@ def process_posts(raw_file_path, processed_file_path=None):
     unified_tags = get_unified_tags(enriched_posts)
     for post in enriched_posts:
         current_tags = post['tags']
-        new_tags = {unified_tags[tag] for tag in current_tags if tag in unified_tags}
+        # new_tags = {unified_tags[tag] for tag in current_tags if tag in unified_tags}
+        new_tags = {tuple(unified_tags[tag]) for tag in current_tags if tag in unified_tags}
+
         post['tags'] = list(new_tags)
     with open(processed_file_path, encoding='utf-8', mode="w") as outfile:
       json.dump(enriched_posts, outfile, indent=4)
